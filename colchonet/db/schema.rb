2537ab23bc7c7e0e3b817bc98683827858e151da
jhoestevam <jhonatanm222@gmail.com>
2018-03-06 00:26:54 -0300

    Model Usuario, validacoes, nova gem de validação de senha e view editar mostrar e criar novos usuarios

--- colchonet/db/schema.rb
+++ colchonet/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222220335) do
+ActiveRecord::Schema.define(version: 20180306023823) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -24,11 +24,11 @@ ActiveRecord::Schema.define(version: 20180222220335) do
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
